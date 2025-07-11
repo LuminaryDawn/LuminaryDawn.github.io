c479c11b990fdef0f2699d86d316968bf06be8ff
LuminaryDawn <3275493671@qq.com>
2025-07-11 14:05:59 +0800

    Site updated: 2025-07-11 14:05:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/09/hello-world/","2025/07/10/初识JAVA/"];function toRandomPost(){
+var posts=["2025/07/09/hello-world/","2025/07/10/初识JAVA/","2025/07/11/安卓逆向入门笔记/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
