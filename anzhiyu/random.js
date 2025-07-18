405d162746b01bbfb18b0717515ee9e3610e270a
LuminaryDawn <3275493671@qq.com>
2025-07-18 14:56:04 +0800

    Site updated: 2025-07-18 14:56:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/09/hello-world/","2025/07/10/初识JAVA/","2025/07/11/安卓逆向入门笔记/","2025/07/14/逆向题目题解/","2025/07/17/初探逆向/"];function toRandomPost(){
+var posts=["2025/07/09/hello-world/","2025/07/10/初识JAVA/","2025/07/11/安卓逆向入门笔记/","2025/07/14/逆向题目题解/","2025/07/17/初探逆向/","2025/07/18/TEa加密/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
